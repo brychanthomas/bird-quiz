8ef99e9ae2a7235463c009c3b896726630eb4d04
Brychan Thomas <brychan.thomas@btinternet.com>
2021-12-31 18:51:18 +0000

    Added more birds

--- src/birdLists.ts
+++ src/birdLists.ts
@@ -8,13 +8,51 @@ export var birdLists = [
     ]
   },
   {
-    "name":"Tits",
+    "name":"Tits and long-tailed tits",
     "birds":[
       ["Blue tit"],
       ["Great tit"],
       ["Coal tit"],
       ["Willow tit"],
-      ["Marsh tit"]
+      ["Marsh tit"],
+      ["Long-tailed tit", "Long tailed tit"]
+    ]
+  },
+  {
+    "name":"Crows",
+    "birds":[
+      ["Eurasian jay", "Jay"],
+      ["Black-billed magpie", "Magpie"],
+      ["Spotted nutcracker", "Nutcracker"],
+      ["Red-billed chough", "Chough"],
+      ["Eurasian jackdaw", "Jackdaw"],
+      ["Rook"],
+      ["Carrion crow"],
+      ["Hooded crow"],
+      ["Common raven", "Raven"]
+    ]
+  },
+  {
+    "name":"Sparrows",
+    "birds":[
+      ["House sparrow"],
+      ["Eurasian tree sparrow", "Tree sparrow"]
+    ]
+  },
+  {
+    "name":"Old World flycatchers",
+    "birds":[
+      ["European Robin", "Robin"]
+    ]
+  },
+  {
+    "name":"Pigeons and doves",
+    "birds":[
+      ["Rock dove", "Pigeon"],
+      ["Stock dove"],
+      ["Common woodpigeon", "Woodpigeon"],
+      ["Eurasian collared dove", "Collared dove"],
+      ["European turtle dove", "Turtle dove"],
     ]
   }
 ];
